7509f36e46c3ebacbaaaffa111a1e0ce33b3d0f8
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-08 15:11:21 -0300

    Multidimensional Arrays complete

---
+++ multidimensional_arrays/arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int numbers[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d -> ", numbers[i][j]);
+        }
+    }
+
+    printf("End\n");
+
+    return 0;
+}
