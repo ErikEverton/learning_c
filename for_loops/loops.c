50c7b57740be21bcd57725271e7071c4a3023bc3
ErikEverton <122756143+ErikEverton@users.noreply.github.com>
2024-02-09 20:49:26 +0000

    For Loops complete

---
+++ for_loops/loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sum = 0;
+    int i;
+    for (i = 0; i < 10; i++) {
+        sum += array[i];
+    }
+    printf("Sum of the array is %d\n", sum);
+    return 0;
+}
