c30395ab62459d43c08ebf1395d84c77968dc438
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-13 15:19:23 -0300

    Insert at end logic added

--- linked_lists/main.c
+++ linked_lists/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct node {
     int value;
     struct node * next;
 } node;
+
+
+void insert_at_end(node * head, int val); 
 
 
 int main(void) {
@@ -17,6 +20,22 @@ int main(void) {
     head->value = 0;
     head->next = NULL;
 
+    for (int i = 1; i <= 10; i++) {
+        insert_at_end(head, i);
+    }
+
     printf("the head value is %d\n", head->value);
     return 0;
+}
+
+
+void insert_at_end(node * head, int val) {
+    node * current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = (node *) malloc(sizeof(node));
+    current->next->value = val;
+    current->next->next = NULL;
 }
