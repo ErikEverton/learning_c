33795627a36655d6511b9c38c780747dedf4e11d
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-13 15:13:05 -0300

    List first item, called head added

--- linked_lists/main.c
+++ linked_lists/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 typedef struct {
     int value;
@@ -7,4 +7,16 @@ typedef struct {
 } node;
 
 
+int main(void) {
+    node * head = (node *) malloc(sizeof(node));
 
+    if (head == NULL) {
+        return 1;
+    }
+
+    head->value = 0;
+    head->next = NULL;
+
+    printf("the head value is %d\n", head->value);
+    return 0;
+}
