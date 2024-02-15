b2450110c655e2eda2cfca27c557b4cdf22eea1e
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-15 09:54:57 -0300

    find max function added and fixing memory leaks

--- binary_search_tree/main.c
+++ binary_search_tree/main.c
@@ -9,6 +9,8 @@ typedef struct node {
 struct node * create_node(int value);
 struct node * delete(node * root, int target);
 struct node * find_min(node * root);
+struct node * find_max(node * root);
+struct node * free_tree(node * root);
 struct node * insert(node * root, int value);
 struct node * search(node * root, int target);
 
@@ -45,7 +47,10 @@ int main(void) {
         }
     }
 
+    node * max = find_max(root);
 
+    printf("MAX: %d\n", max->key);
+    free_tree(root);
 
     return 0;
 }
@@ -100,6 +105,36 @@ struct node * find_min(node * current) {
         return find_min(current->left);
     }
     return current;
+}
+
+
+struct node * find_max(node * current) {
+    if (current == NULL) {
+        return NULL;
+    } else if (current->right != NULL) {
+        return find_max(current->right);
+    }
+    return current;
+}
+
+
+struct node * free_tree(node * current) {
+    if (current->left == NULL && current->right == NULL) {
+        free(current);
+        return NULL;
+    }
+
+    if (current->left != NULL && current->right == NULL) {
+        free_tree(current->left);
+        free(current);
+    } else if (current->left == NULL && current->right != NULL) {
+        free_tree(current->right);
+        free(current);
+    } else {
+        return free_tree(current->left);
+    }
+    
+    return NULL;
 }
 
 
